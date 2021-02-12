736907af105ed4c6db29fc6a4ef01501ea77e898
Valerie Hammond <luckbit4755+git-em@gmail.com>
2021-02-12 15:49:11 -0500

    cleanup script a bit

--- opt/kvm-console/bin/viewer-app.sh
+++ opt/kvm-console/bin/viewer-app.sh
@@ -14,72 +14,91 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ip_address="${IPMI_ADDRESS}"
-username=${IPMI_USERNAME:-ADMIN}
-password=${IPMI_PASSWORD:-ADMIN}
+export PROTOCOL="https"
 
-get_session_id() {
-  local login_url="${proto}://${ip_address}/cgi/login.cgi"
-  curl --connect-timeout 2 --insecure -s -X POST "${login_url}" --data "name=${username}&pwd=${password}" -i | awk '/SID=[^;]/ { print $2 }'
+_main() {
+	#############################################################################
+	# get the session id
+
+	local login_url="${PROTOCOL}://${IPMI_ADDRESS}/cgi/login.cgi"
+	echo "fetching the session id from ${login_url}"
+
+	session_id=$(get_session_id)
+	if [ -z "${session_id}" ]; then
+		echo "could not fetch the session id with ${PROTOCOL}, trying http"
+		export PROTOCOL="http"
+	  	session_id=$(get_session_id)
+	fi
+	if [ -z "${session_id}" ]; then
+		echo "could not get the session id with https or http"
+		exit 1
+	fi
+
+	set -e
+	set -o pipefail
+
+	#############################################################################
+	# Download jnlp
+	echo Download jnlp
+
+	mkdir -p "workspace-${IPMI_ADDRESS}"
+	cd "workspace-${IPMI_ADDRESS}"
+	jnlp_url="${PROTOCOL}://${IPMI_ADDRESS}/cgi/url_redirect.cgi?url_name=ikvm&url_type=jwsk"
+	curl -s --insecure "${jnlp_url}" -H 'Referer: http://localhost' -H "Cookie: ${session_id}" > launch.jnlp 
+	if [ 0 != ${?} ] ; then
+		echo failed to download from ${jnlp_url} 
+		exit 2
+	fi
+
+	# Download resource
+	echo Download resource
+
+	codebase_url=$(xmlstarlet sel -t -v '/jnlp/@codebase' launch.jnlp)
+	jars="$(xmlstarlet sel -t -v 'concat(//*/jar/@href, //*/jar/@version)' launch.jnlp | sed 's/.jar\(..*\)$/__V\1.jar/g')"
+	query='@os="'"$(uname | sed 's/Darwin/Mac OS X/')"'" and @arch="'"$(uname -m)"'"'
+	libs="$(xmlstarlet sel -t -v 'concat(//*/resources['"$query"']/nativelib/@href, //*/nativelib[position()]/@version)' launch.jnlp | sed 's/.jar\(..*\)$/__V\1.jar/g')"
+
+	for resource in $jars $libs; do
+		url="${codebase_url}/${resource}.pack.gz"
+		if [ -f ${resource} ] ; then
+			echo already downloaded ${resource}: $( ls -l ${resource} )
+		else
+			tmp_file="viewer-app.tmp"
+			echo downloading $resource from ${url}
+			curl --insecure -L -H 'Referer: http://localhost' -H "Cookie: ${session_id}" ${url} > ${tmp_file} || exit 3
+			echo 
+			ls -l ${tmp_file}
+			echo unpack
+			unpack200 ${tmp_file} $resource || return 
+			echo unpacked
+			
+		fi
+		#[ -f $resource ] || curl --insecure -L -H 'Referer: http://localhost' -H "Cookie: ${session_id}" "${codebase_url}/${resource}.pack.gz" | unpack200 - $resource
+	done
+
+	# Extract libraries
+	echo Extract libraries
+
+	for lib in $libs; do
+		unzip -o $lib > /dev/null || exit 4
+	done
+
+	# Start application
+	echo Start application
+
+	java_vm_args=$(xmlstarlet sel -t -v '//*/j2se/@java-vm-args' launch.jnlp || true)
+	main_class=$(xmlstarlet sel -t -v '//*/application-desc/@main-class' launch.jnlp)
+	arguments=$(xmlstarlet sel -t -v '//*/application-desc/argument' launch.jnlp)
+	exec java -Djava.library.path=. -cp $(echo $jars|tr ' ' ':') $java_vm_args $main_class $arguments
 }
 
-proto=https
-session_id=$(get_session_id)
-if [ -z "${session_id}" ]; then
-  proto=http
-  session_id=$(get_session_id)
-fi
+get_session_id() {
+	local username=${IPMI_USERNAME:-ADMIN}
+	local password=${IPMI_PASSWORD:-ADMIN}
 
-[ "${session_id}" ] || exit 1
+	local login_url="${PROTOCOL}://${IPMI_ADDRESS}/cgi/login.cgi"
+	curl --connect-timeout 2 --insecure -s -X POST "${login_url}" --data "name=${username}&pwd=${password}" -i \
+	| awk '/SID=[^;]/ { print $2 }'
+}
 
-set -e
-set -o pipefail
-
-# Download jnlp
-echo Download jnlp
-
-mkdir -p "workspace-${ip_address}"
-cd "workspace-${ip_address}"
-jnlp_url="${proto}://${ip_address}/cgi/url_redirect.cgi?url_name=ikvm&url_type=jwsk"
-curl -s --insecure "${jnlp_url}" -H 'Referer: http://localhost' -H "Cookie: ${session_id}" > launch.jnlp || exit 2
-
-# Download resource
-echo Download resource
-
-codebase_url=$(xmlstarlet sel -t -v '/jnlp/@codebase' launch.jnlp)
-jars="$(xmlstarlet sel -t -v 'concat(//*/jar/@href, //*/jar/@version)' launch.jnlp | sed 's/.jar\(..*\)$/__V\1.jar/g')"
-query='@os="'"$(uname | sed 's/Darwin/Mac OS X/')"'" and @arch="'"$(uname -m)"'"'
-libs="$(xmlstarlet sel -t -v 'concat(//*/resources['"$query"']/nativelib/@href, //*/nativelib[position()]/@version)' launch.jnlp | sed 's/.jar\(..*\)$/__V\1.jar/g')"
-
-for resource in $jars $libs; do
-	url="${codebase_url}/${resource}.pack.gz"
-	if [ -f ${resource} ] ; then
-		echo already downloaded ${resource}: $( ls -l ${resource} )
-	else
-		tmp_file="viewer-app.tmp"
-		echo downloading $resource from ${url}
-		curl --insecure -L -H 'Referer: http://localhost' -H "Cookie: ${session_id}" ${url} > ${tmp_file} || exit 3
-		echo 
-		ls -l ${tmp_file}
-		echo unpack
-		unpack200 ${tmp_file} $resource || return 
-		echo unpacked
-		
-	fi
-    #[ -f $resource ] || curl --insecure -L -H 'Referer: http://localhost' -H "Cookie: ${session_id}" "${codebase_url}/${resource}.pack.gz" | unpack200 - $resource
-done
-
-# Extract libraries
-echo Extract libraries
-
-for lib in $libs; do
-    unzip -o $lib > /dev/null || exit 4
-done
-
-# Start application
-echo Start application
-
-java_vm_args=$(xmlstarlet sel -t -v '//*/j2se/@java-vm-args' launch.jnlp || true)
-main_class=$(xmlstarlet sel -t -v '//*/application-desc/@main-class' launch.jnlp)
-arguments=$(xmlstarlet sel -t -v '//*/application-desc/argument' launch.jnlp)
-exec java -Djava.library.path=. -cp $(echo $jars|tr ' ' ':') $java_vm_args $main_class $arguments
+_main
